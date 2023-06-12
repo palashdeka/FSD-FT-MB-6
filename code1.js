1a08c01deb33c039826e5b9e1416eb5a9a45f6af
PalashDeka <86890811+palashdeka@users.noreply.github.com>
2023-06-12 10:53:53 +0530

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,12 @@
+//print numbers from 1 to 10
+
+//for(let i = 1; i<=10; i++){
+  //  document.write(i+"<br>")
+//}
+
+
+//print the odd numbers less than 100
+
+for(let i =1; i<=100; i+=2){
+    document.write(i+"<br>")
+}
